930661bab7deb60ccfa9c0105f4561c7a55ea8c7
kevinlim186 <kevinlim186@gmail.com>
2020-05-03 11:19:30 +0200

    created experiment setup

--- Main.py
+++ Main.py
@@ -1,13 +1,12 @@
 from pflacco.pflacco import create_initial_sample, create_feature_object, calculate_feature_set, calculate_features
-import bbobbenchmarks.bbobbenchmarks as bn
 from modea import Algorithms, Parameters, Individual, Mutation, Utils, Recombination, Selection, Sampling
 from modea.Utils import getOpts, getVals, reprToString, options, initializable_parameters, ESFitness
 import os
 import numpy as np
-from functools import partial
-from Util import createBBOBFunction, runOneGeneration, initializePopulation, graph2DFunction
+from Util import createBBOBFunction, runOneGeneration, initializePopulation, graph2DFunction, performanceLogger,ensureFullLengthRepresentation, computeERT
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 #Config
 n=2
@@ -16,81 +15,40 @@ l_bound=-5
 u_bound=5
 mu=20
 lambda_=20
+directory = './results/'
 
 #initiate the function
 function = createBBOBFunction(functionID=0, instanceNumber=44)
 
 #graph the function
+
 resolution = 0.1
-graph2DFunction(lbound=l_bound, ubound=u_bound,resolution=resolution, function=function['function'])
+graph2DFunction(lbound=l_bound, ubound=u_bound,resolution=resolution, function=function['function'], functionName=function['name'], directory = directory+function['name']+'.png')
 
 
-# We use functions here to 'hide' the additional passing of parameters that are algorithm specific
-parameters = Parameters.Parameters(n=n, budget=budget,l_bound=l_bound, u_bound=u_bound, mu=mu, lambda_=lambda_)
-recombine = Recombination.random
-mutate = partial(Mutation.CMAMutation, sampler=Sampling.GaussianSampling(2))
-select = Selection.onePlusOneSelection
-mutateParameters = parameters.oneFifthRule
-population = initializePopulation(parameters)
+#Customized ES
+starting_configuration = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+configName =  ''.join([str(elem) for elem in starting_configuration]) 
+representation = ensureFullLengthRepresentation(starting_configuration)
 
+# Interpret the representation into parameters for the ES
+opts = getOpts(representation[:len(options)])
+values = getVals(representation[len(options)+2:])
+values = getVals(starting_configuration)
 
-functions = {
-    'recombine': recombine,
-    'mutate': mutate,
-    'select': select,
-    'mutateParameters': mutateParameters,
-}
+# Initialize the algorithm
+customES = Algorithms.CustomizedES(2, function['function'], budget=1000, opts=opts, values=values)
+customES.mutateParameters = customES.parameters.adaptCovarianceMatrix
 
-#EvolutionaryOptimizer (OK)
-evolutionaryOptimizer = Algorithms.EvolutionaryOptimizer(population, function['function'], budget=parameters.budget, functions= functions,parameters=parameters)
-
-generation = runOneGeneration(evolutionaryOptimizer)
-generation['EvolutionaryOptimizer']
-generation['Logger']
-generation['FeatureObj']
-generation['EvolutionaryOptimizer'].population
-
-evolutionaryOptimizer.runOptimizer()
-
-evolutionaryOptimizer.fitness_over_time
-evolutionaryOptimizer.sigma_over_time
-
-
-# Calculate all features
-ela_features = calculate_features(generation['FeatureObj'])
-print(ela_features)
-
-
-#Computing for fitness over time
-fitness = ESFitness(fitnesses=np.array([evolutionaryOptimizer.fitness_over_time]), target=1.565)
-fitness.FCE
-fitness.ERT
+#Get the ERT
+customES, performance = computeERT(customES, target=10e-8)
+performance.to_csv(directory+function['name']+' '+ configName + '.csv')
 
 #graphing for fitness over time
-fitness = pd.DataFrame({'Fitness Over Time': evolutionaryOptimizer.fitness_over_time, 'Sigma Over Time': evolutionaryOptimizer.sigma_over_time})
-    
+fitness = pd.DataFrame({'Fitness Over Time': customES.fitness_over_time, 'Sigma Over Time': customES.sigma_over_time})
+
+plt.clf()
 plot = sns.lineplot( markers=True, data=fitness, palette="tab10", linewidth=2.5)
-plt.show()
+plt.title(directory+function['name']+' '+ configName)
+plt.savefig(directory+function['name']+' '+ configName + '.png')
 
-
-
-
-# Arbitrary objective function
-def objective_function(x, dim):
-    return [entry[0]**2 - entry[1]**2 for entry in x]
-
-
-# Create inital sample using latin hyper cube sampling
-sample = create_initial_sample(100, 2, type = 'lhs')
-# Calculate the objective values of the initial sample using an arbitrary objective function (here y = x1^2 - x2^2)
-obj_values = objective_function(sample, 2)
-# Create feature object
-feat_object = create_feature_object(sample, obj_values, lower=-10,upper=10, blocks=3)
-
-# Calculate a single feature set
-cm_angle_features = calculate_feature_set(feat_object, 'cm_angle')
-
-
-# Calculate all features
-ela_features = calculate_features(feat_object)
-ela_features['gcm.near.basin_prob.sd']
