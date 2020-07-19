870551ec18301eb238fc752eb97786ece7ba127f
kevinlim186 <kevinlim186@gmail.com>
2020-07-19 09:12:53 +0200

    added progress bar for flacco runner

--- flacco_runner.py
+++ flacco_runner.py
@@ -11,11 +11,32 @@ baseDIR = "./temp/"
 files = os.listdir(baseDIR)
 files = [val for val in files if val.endswith("pflacco.csv")]
 
+def _printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 50, fill = '█', printEnd = "\r"):
+	"""
+	Call in a loop to create terminal progress bar
+	@params:
+		iteration   - Required  : current iteration (Int)
+		total       - Required  : total iterations (Int)
+		prefix      - Optional  : prefix string (Str)
+		suffix      - Optional  : suffix string (Str)
+		decimals    - Optional  : positive number of decimals in percent complete (Int)
+		length      - Optional  : character length of bar (Int)
+		fill        - Optional  : bar fill character (Str)
+		printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+	"""
+	percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+	filledLength = int(length * iteration // total)
+	bar = fill * filledLength + '-' * (length - filledLength)
+	print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+	# Print New Line on Complete
+	if iteration == total: 
+		print()
+
 for i in range(len(files)):
 	func= int(files[i].split("_")[1].replace('F',''))
 	dim= int(files[i].split("_")[-6].replace('D',''))
 	filename = files[i].replace('_pflacco.csv', '')
-
+	_printProgressBar(i, len(files))
 	if func in function :
 		problem = Problem(1, func, [1], dim, [0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1], 1, performance,True)
 		problem.currentResults = pd.read_csv(baseDIR+files[i])
