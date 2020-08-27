7118d8a8f6679bd57173375ce6213594da3a90a3
kevinlim186 <kevinlim186@gmail.com>
2020-08-27 21:13:15 +0200

    switched to spaces

--- main_runner.py
+++ main_runner.py
@@ -59,21 +59,21 @@ esconfig = [0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1]
 '''
 #function 6 to 7
 for i in range(6,8):
-	suite = Suites(instances=[1,2,3,4,5], baseBudget=10000, dimensions=[2,3,5,10,20], esconfig=[0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1], function=i, performance=performance, pflacco=False)
+    suite = Suites(instances=[1,2,3,4,5], baseBudget=10000, dimensions=[2,3,5,10,20], esconfig=[0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1], function=i, performance=performance, pflacco=False)
 
-	suite.runDataGathering()
+    suite.runDataGathering()
 '''
 
 '''
 for i in range(1,25):
-	suite = Suites(instances=[6,7,8,9,10], baseBudget=10000, dimensions=[2,3,5,10,20], esconfig=esconfig, function=i, performance=performance, pflacco=False, localSearch=localSearch)
-	suite.runTestSuite()
+    suite = Suites(instances=[6,7,8,9,10], baseBudget=10000, dimensions=[2,3,5,10,20], esconfig=esconfig, function=i, performance=performance, pflacco=False, localSearch=localSearch)
+    suite.runTestSuite()
 
 performance.saveToCSVPerformance('Test_'+name)
 '''
 
 for i in range(1,25):
     suite = Suites(instances=[6,7,8,9,10], baseBudget=10000, dimensions=[2,3,5,10,20], esconfig=esconfig, function=i, performance=performance, pflacco=True, localSearch=None)
-	suite.runTestSuite(ASP=model, size=50,restart=True)
+    suite.runTestSuite(ASP=model, size=50,restart=True)
 
 performance.saveToCSVPerformance('Test_'+name)
