0199ed73b8333363f0513f1bec31063375f17c56
kevinlim186 <kevinlim186@gmail.com>
2020-10-24 19:18:06 +0200

    fixed the sizes in suites

--- src/suites.py
+++ src/suites.py
@@ -28,8 +28,7 @@ class Suites:
 
     def runProblem (self, budget, function, instance, dimension, esconfig, checkPoint, logger, pflacco, localSearch):
             problem = Problem(budget, function, instance, dimension, esconfig, checkPoint, logger,pflacco, localSearch)
-            genSize = 4+ math.floor(3*log(dimension))
-            problem.runDataGathering(size= [50*dimension,100*dimension,200*dimension, genSize*5, genSize*50, genSize*100])
+            problem.runDataGathering(size= [50,100,200, '5G', '50G', '100G'])
             #problem.saveElaFeat('Function_'+str(function))
 
     def runTestSuite(self):
