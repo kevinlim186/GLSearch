reverted back to the correct FCE computation

src/result.py
@@ -105,8 +105,7 @@ class Result():
 
         #adjust FCE to factor in accuracy set when experiment was setup
         self.processedPerformance['fce'] =  self.processedPerformance['fce']+ 1e-8 
-        self.processedPerformance['fceTarget'] = self.processedPerformance.apply(lambda x: self._getOptimalValue(x['function'], x['instance']), axis=1)
-        self.processedPerformance['relFCE'] = self.processedPerformance.apply(lambda x: 1 + ((np.log10(float(x['fce'])/x['fceTarget']))/(np.log10(float(x['fcemax'])/x['fceTarget'])) ), axis=1)
+        self.processedPerformance['relFCE'] = self.processedPerformance.apply(lambda x: 1 + ((np.log10(float(x['fce'])/1e-8))/(np.log10(float(x['fcemax'])/1e-8)) ), axis=1)
 
         #Calculate performance based on Rajn's performance measure; 
         self.processedPerformance['performance'] = self.processedPerformance.apply(lambda x: x['relERT'] if x['relERT'] >0  else x['relFCE'], axis=1)