cbfff2cac22e1719004ad7989bac6d4c653d3f97
kevinlim186 <kevinlim186@gmail.com>
2020-11-15 10:17:06 +0100

    added Config

---
+++ src/config.py
@@ -0,0 +1,17 @@
+config = {}
+config['host'] =  "yellowplannet.com"
+config['dbuser'] = 'root'
+config['dbpassword'] = ''
+config['database'] = 'master_thesis'
+
+config['sshHost'] = 'yellowplannet.com'
+config['sshUser'] = 'kevin'
+config['sshPass'] = ''
+
+config['allowSSH'] = 'False'
+config['allowSql'] = 'False'
+
+ #choose between local using docker or remote using CSV or server with local SQL
+config['port'] = 3306
+
+config['flacco'] = 'False'
